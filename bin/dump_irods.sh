7cba4486504ff031dc58409d4247c4335234ced5
wikiselev <vk6@sanger.ac.uk>
2018-02-13 15:37:59 +0000

    add cram2fastq process

--- bin/dump_irods.sh
+++ bin/dump_irods.sh
@@ -1,6 +1,5 @@
 # make directory for each run_lane
 run_lane="$1"
-mkdir $run_lane
 
 # extract run and lane
 run=`echo $run_lane | sed -e 's/_.*//'`
@@ -9,17 +8,17 @@ lane=`echo $run_lane | sed -e 's/.*_//'`
 # get the cram files
 imeta qu -z seq -d id_run = $run and lane = $lane and target = 1 and type = cram \
 | grep : | awk '{ print $2 }' | paste - - -d/ \
-| xargs -ixxx iget -K xxx $run_lane
+| xargs -ixxx iget -K xxx ./
 
-chmod 664 $run_lane/*
+chmod 664 *
 
 # remove phiX control
-find $run_lane | grep -E '#888\.' | xargs rm
+find ./ | grep -E '#888\.' | xargs rm
 
 # get and format the meta info.
-for cram in $(find $run_lane | grep cram$ | sed -e 's/.*\///' | sed -e 's/\.cram$//'); do
-    imeta ls -d /seq/$run/$cram.cram > $run_lane/$cram.imeta
-    sn=$(grep -A 1 sample_supplier_name $run_lane/$cram.imeta | tail -1 | sed 's/ /_/g')
+for cram in $(find ./ | grep cram$ | sed -e 's/.*\///' | sed -e 's/\.cram$//'); do
+    imeta ls -d /seq/$run/$cram.cram > $cram.imeta
+    sn=$(grep -A 1 sample_supplier_name $cram.imeta | tail -1 | sed 's/ /_/g')
     sample_name=${sn:7}
-    echo -e "$run_lane/$cram\t$sample_name" >> $run_lane/sampleInfo.txt
+    echo -e "$run_lane/$cram\t$sample_name" >> $run_$lane_sampleInfo.txt
 done
