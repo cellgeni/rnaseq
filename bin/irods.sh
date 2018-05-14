b071195ed7e06b76c50a7fbced150e6703c0746e
wikiselev <vk6@sanger.ac.uk>
2018-05-14 21:18:17 +0100

    Stijn, please don't laugh at me!

--- bin/irods.sh
+++ bin/irods.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-imeta qu -z seq -d id_run = $1 and target = 1 and manual_qc = 1 \
+imeta qu -z seq -d sample = $1 and target = 1 and manual_qc = 1 \
 | perl -0777 -ne 'while (/collection:\s*(\S+)\ndataObj:\s*(\S+)/gs) { print "iget -K $1/$2\n" }' \
 | bash
