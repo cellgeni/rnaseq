3b4ea1f7fda0e27e51c77bdcaec3f5b2243cc80e
wikiselev <vk6@sanger.ac.uk>
2018-05-14 21:24:46 +0100

    add empty sample check

--- bin/irods.sh
+++ bin/irods.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-imeta qu -z seq -d sample = $1 and target = 1 and manual_qc = 1 \
-| perl -0777 -ne 'while (/collection:\s*(\S+)\ndataObj:\s*(\S+)/gs) { print "iget -K $1/$2\n" }' \
-| bash
+IMETA_OUTPUT="$(imeta qu -z seq -d sample = $1 and target = 1 and manual_qc = 1)"
+if [ "$(echo $IMETA_OUTPUT)" != 'No rows found' ]; then
+    echo "${IMETA_OUTPUT}" \
+    | perl -0777 -ne 'while (/collection:\s*(\S+)\ndataObj:\s*(\S+)/gs) { print "iget -K $1/$2\n" }' \
+    | bash
+fi
