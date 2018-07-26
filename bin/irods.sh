b49cbcb8a8833578688855e7b02e14e641609e6e
wikiselev <vk6@sanger.ac.uk>
2018-07-26 15:12:20 +0100

    add and

--- bin/irods.sh
+++ bin/irods.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-IMETA_OUTPUT="$(imeta qu -z seq -d study_id = $1 sample = $2 and target = 1 and manual_qc = 1)"
+IMETA_OUTPUT="$(imeta qu -z seq -d study_id = $1 and sample = $2 and target = 1 and manual_qc = 1)"
 if [ "$(echo $IMETA_OUTPUT)" != 'No rows found' ]; then
     echo "${IMETA_OUTPUT}" \
     | perl -0777 -ne 'while (/collection:\s*(\S+)\ndataObj:\s*(\S+)/gs) { print "iget -K $1/$2\n" }' \
