2b1c7a240bc4e335c5b9bd89ae282838b23c0cbc
Stijn van Dongen <micans@users.noreply.github.com>
2018-07-30 15:03:05 +0100

    Support multiple table types with merge_featurecounts.py

--- bin/merge_featurecounts.py
+++ bin/merge_featurecounts.py
@@ -2,11 +2,13 @@
 
 import argparse
 import os
+import sys
 import re
 import logging
 from collections import defaultdict
 
 def merge_featureCounts(dest_dir,out_file,input_files):
+
    logger = logging.getLogger(__name__)
    logger.addHandler(logging.StreamHandler())
    logger.setLevel(logging.INFO)
@@ -17,20 +19,31 @@ def merge_featureCounts(dest_dir,out_file,input_files):
    for input_file in input_files:
        logger.info("Reading from {}".format(input_file))
        sample_name=os.path.basename(input_file)
-       sample_name= sample_name.replace("Aligned.sortedByCoord.out_gene.featureCounts.txt","")
+       if (len(thesuffix)):
+           sample_name = sample_name.replace(thesuffix,"")
+           # featureCounts uses      Aligned.sortedByCoord.out_gene.featureCounts.txt
+           # salmon uses             _1.quant.genes.sf
        sample_names.append(sample_name)
        table_dict[sample_name]=dict()
+       must_read_header = expect_header
      
        with open(input_file, 'r') as f:
-           f.readline()
-           f.readline()
+           while must_read_header:
+               line = f.readline()
+               if skip_comments and re.search("^\s*#", line):
+                   continue
+               else:
+                   break        # only a single-line header supported currently, disregarding comments.
+
            for line in f:
+               if skip_comments and re.search("^\s*#", line):
+                   continue
                #save the genes to a list for the first file
                line_info=line.split('\t')
                gene=line_info[0]
                genes.add(gene)
                try:
-                   gene_count = int(line_info[-1].rstrip())
+                   gene_count = line_info[thecolumn].rstrip()
                    table_dict[sample_name][gene] = gene_count
                except TypeError:
                        logger.warning("Detected discrepancy in {}  line {}".format(input_file, line))
@@ -66,9 +79,26 @@ if __name__ == "__main__":
     """)
     parser.add_argument("-d", "--dest_dir", dest='dest_dir', default='.',
                                    help="Path to output.")
+    parser.add_argument("-c", dest='colidx', default=-1, type=int,
+                                   help="Index of column containing counts")
+    parser.add_argument("--rm-suffix", default='',
+                                   help="Remove suffix from file name to obtain sample name")
+    parser.add_argument("--skip-comments", action="store_true",
+                                   help="skip lines starting with #")
+    parser.add_argument("--header", action="store_true",
+                                   help="first non-commment line is a header line")
     parser.add_argument("-o", "--results_file_name",dest='out_file', default='all_counts.txt',
                                    help= "Name of the output file that will be created")
     parser.add_argument("-i", "--input_files", metavar='<input_files>', nargs='+', default='*.featureCounts.txt',
                                    help="Path to the outputfiles from FeatureCounts. ")
     args = parser.parse_args()
+    skip_comments = args.skip_comments
+    expect_header = args.header
+    thesuffix = args.rm_suffix
+    thecolumn = args.colidx
+
     merge_featureCounts(args.dest_dir, args.out_file, args.input_files)
+
+
+
+
